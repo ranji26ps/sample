dc4e21ee236cacc9ce122398c169d27135179b89
ranji26ps <psranjith517@gmail.com>
2024-10-29 15:30:22 +0530

    add function to check for anagrams between two strings

---
+++ 13th ans.py
@@ -0,0 +1,9 @@
+str1 = input("enter string 1")
+str2 = input("enter string 2")
+if len (str1) == len(str2):
+   print ("not anagrams")
+else:
+   if sorted (string1)==sorted(str2):
+      print("strings are anagrams")
+   else:
+      print("Not anagrams")
