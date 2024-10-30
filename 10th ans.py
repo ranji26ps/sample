a0b270a5901ada0f7848a8f2af8a6c53aab8c262
ranji26ps <psranjith517@gmail.com>
2024-10-30 10:55:36 +0530

    Add function to print multiples from 1 to 100 updated

--- 10th ans.py
+++ 10th ans.py
@@ -1,11 +1,15 @@
-def fizzbuzzfxn (lowerlimit=1, Upperlimit=100):
-    for i in range(lowerlimit,Upperlimit +1):
-     if i % 15 == 0 :
-        print ("fizzbuzz")
-     elif i % 3 == 0 :
-        print ("fizz")
-    else:
-       print(i)
+def fizzbuzzfxn(lowerlimit=1, upperlimit=100):
+    for i in range(lowerlimit, upperlimit + 1):
+        if i % 15 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:  
+            print("buzz")
+        else:
+            print(i)
+
 fizzbuzzfxn()
-fizzbuzzfxn(4,7)
+fizzbuzzfxn(4, 7)
+
 
