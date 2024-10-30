9edcfa15a5b7da47ebb12aa9ded75fa4dcf5d9d8
ranji26ps <psranjith517@gmail.com>
2024-10-30 10:58:46 +0530

    implement function to find the second largest number in a list updated

--- 12th ans.py
+++ 12th ans.py
@@ -1,13 +1,23 @@
-length = int(input("enter a list length:"))
+length = int(input("Enter a list length: "))
 numbers_list = []
+
 for i in range(length):
-  temp = int(input("Enter a list item: "))
-numbers_list.append(temp)
-first_largest,second_largest = numbers_list[0],numbers_list[0]
-for item in numbers_list:
-    if item > first_largest:
-        second_largest = first_largest
-        first_largest = item
-    elif item > second_largest and item == first_largest:
-                     second_largest = item
-print ("second_largest number in a list :",second_largest)
+    temp = int(input("Enter a list item: "))
+    numbers_list.append(temp)  # Move this line inside the loop
+
+if len(numbers_list) < 2:
+    print("List needs at least two distinct numbers.")
+else:
+    first_largest, second_largest = float('-inf'), float('-inf')
+
+    for item in numbers_list:
+        if item > first_largest:
+            second_largest = first_largest
+            first_largest = item
+        elif item > second_largest and item != first_largest:  
+            second_largest = item
+
+    if second_largest == float('-inf'):
+        print("There is no second largest number (all numbers might be the same).")
+    else:
+        print("Second largest number in the list:", second_largest)
