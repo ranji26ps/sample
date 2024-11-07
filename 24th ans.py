f85137ee305a9d68576734f8d9388e92a7de31d8
Ranjith <psranjith517@gmail.com>
2024-11-07 15:00:56 +0530

    statements to check if the numper is divisible

---
+++ 24th ans.py
@@ -0,0 +1,13 @@
+a = int (input("enter a number"))
+if a % 2 == 0:
+    print ("it is divisible by 2")
+else:
+    print ("it is not divisible by 2")
+if a % 3 == 0:
+    print ("it is divisible by 3")
+else:
+    print ("it is not divisible by 3")
+if a % 5 == 0 :
+    print ("it is divisible by 5")
+else:
+    print ("it is not divisible by 5")
