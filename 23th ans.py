7f0e1d488af24b8bf54a1a1bd936b0aad1892f60
Ranjith <psranjith517@gmail.com>
2024-11-07 14:57:42 +0530

    assigned a letter grade based on the score

---
+++ 23th ans.py
@@ -0,0 +1,12 @@
+a = int(input("enter your score"))
+
+if  a > 90:
+    print ("A")
+if a < 90 and a > 80 :
+    print ("B")
+if a < 80 and a > 70 :
+    print ("C")
+if a < 70 and a > 60 :
+    print ("D")
+if a < 60 :
+    print ("F")
