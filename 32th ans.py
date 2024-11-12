3507323d5d6731aef1cb40fd72bbcf5331620a39
Ranjith <psranjith517@gmail.com>
2024-11-12 12:52:42 +0530

    aray

--- 32th ans.py
+++ 32th ans.py
@@ -1,26 +1,6 @@
-def second_largest(arr):
-    if len(arr) < 2:
-        return -1
-    
-    largest = second_largest = float('-inf')  
-    
-    for num in arr:
-        if num > largest:
-            second_largest = largest
-            largest = num
-        elif num > second_largest and num != largest:
-            second_largest = num
-
-    return second_largest if second_largest != float('-inf') else -1
-
-
-
-arr = [23, 12, 1, 10, 34, 13]
-print(second_largest(arr))  
-
-arr = [1, 1, 1, 1]
-print(second_largest(arr))  
-
-arr = [9]
-print(second_largest(arr))  
-
+arr = [12,35,1,10,34,1]
+lar = arr[0]
+for i in range (1,len(arr)):
+    if arr[i] > lar:
+       lar = arr[i]
+print(lar)
