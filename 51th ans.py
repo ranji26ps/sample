c99ffdb41042b46b3918978035ebebd09982ebe8
Ranjith <psranjith517@gmail.com>
2024-11-21 12:45:09 +0530

    'gretst

---
+++ 51th ans.py
@@ -0,0 +1,8 @@
+def greatest(num1,num2,num3):
+    if num1 > num2 and num1 > num3:
+        print("num1 is gratest value")
+    if num2 > num1 and num2 > num3:
+        print("num2 is gretest value")
+    if num3 > num1 and num3 > num2:
+        print("num3 it is gretest value")
+greatest(3,4,6)
