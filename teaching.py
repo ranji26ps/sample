d82579aa7f249ee8fecac74f9e0afa95de41e4a8
ranji26ps <psranjith517@gmail.com>
2024-11-29 16:31:50 +0530

    class with function atributes

--- teaching.py
+++ teaching.py
@@ -1,1 +1,19 @@
-print("Ranjith")
+class Bank_holder():
+    def __init__(self,ah,bm,):
+        self.acount_holder = ah
+        self.bank_methord = bm
+        self.balance = 0
+    def deposit(self,a):
+        if a > 0:
+            self.balance = self.balance+a
+            print(self.balance)
+    def withdraw (self,a):
+        if self.balance > a:
+           self.balance = self.balance - a
+           print(self.balance)
+        else:
+            print("Insufficient funds")
+a1 = Bank_holder("Ranjith","saving")
+a1.deposit(444)
+a1.withdraw(500)
+                    
