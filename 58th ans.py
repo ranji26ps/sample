f92f0d0607e7cc21f02b26f81d5807c769217fd8
ranji26ps <psranjith517@gmail.com>
2024-12-09 15:51:54 +0530

    sum of

---
+++ 58th ans.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter the first number"))
+num2 = float(input("enter the second number"))
+sum = num1 + num2
+deff = num1 - num2
+mult = num1 * num2
+divide = num1 / num2
+print(sum)
+print(deff)
+print(mult)
+print(divide)
